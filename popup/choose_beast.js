a83a693634974fd471b8912826bb5924cb0d9238
ojchase <ojchase@gmail.com>
2021-01-31 00:46:41 -0700

    Have the main scripts load with the okcupid page as opposed to being executed from the popup.
    The popup can still send messages. I like knowing everything loaded together at the same time and I can count on jquery already existing, etc.  And also only having to worry about the code loading once.
    
    This also prevents the error case from happening, eliminating the need to catch it.
    
    Note: this might be the wrong choice for Chrome. See https://stackoverflow.com/questions/46221644/does-a-web-extension-need-to-explicitly-load-content-scripts

--- popup/choose_beast.js
+++ popup/choose_beast.js
@@ -54,6 +54,7 @@ function listenForClicks() {
     /**
      * Get the active tab,
      * then call "beastify()" or "reset()" as appropriate.
+     * The script is already loaded with the page from the manifest.
      */
     if (e.target.classList.contains("beast")) {
       browser.tabs.query({active: true, currentWindow: true})
@@ -68,21 +69,5 @@ function listenForClicks() {
   });
 }
 
-/**
- * There was an error executing the script.
- * Display the popup's error message, and hide the normal UI.
- */
-function reportExecuteScriptError(error) {
-  document.querySelector("#popup-content").classList.add("hidden");
-  document.querySelector("#error-content").classList.remove("hidden");
-  console.error(`Failed to execute beastify content script: ${error.message}`);
-}
+listenForClicks();
 
-/**
- * When the popup loads, inject a content script into the active tab,
- * and add a click handler.
- * If we couldn't inject the script, handle the error.
- */
-browser.tabs.executeScript({file: "/content_scripts/beastify.js"})
-.then(listenForClicks)
-.catch(reportExecuteScriptError);
