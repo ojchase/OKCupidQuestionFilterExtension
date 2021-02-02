5ae26d57d17ce2013afd09e7735c3f62ec1ec2a6
ojchase <ojchase@gmail.com>
2021-02-02 00:31:19 -0700

    Better proof of concept for CSV/JSON conversion. Read, add, edit, and save (not to disk) the question config.

--- content_scripts/beastify.js
+++ content_scripts/beastify.js
@@ -21,9 +21,30 @@ fetch(filePath)
 	}
 
 function doStuff(text){
-let x = Papa.parse(text);
-console.log(x);
-//var results = Papa.parse(csvString, config);
+	let x = Papa.parse(text, {header: true, skipEmptyLines: true});
+	console.log(x);
+	console.log(`There are ${x.data.length} items in the question set`);
+	let headers = x.meta.fields;
+	for(question of x.data){
+		console.log(`${headers[1]} question?: ${question[headers[1]]} (${question.QuestionText}`);
+	}
+	
+	
+	let newQuestionText = "this is a test question";
+	let newQuestionSpiritual = "FALSE";
+	let newQuestion = {
+		
+	}
+	newQuestion[headers[0]] = newQuestionText;
+	newQuestion[headers[1]] = newQuestionSpiritual;
+	x.data.push(newQuestion);
+	
+	let y = Papa.unparse({
+		fields: headers,
+		data: x.data
+	},{headers: true});
+	console.log("New file:")
+	console.log(y);
 }
 
   /**
