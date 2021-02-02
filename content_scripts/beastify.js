79c997f62e38dc80d81326ebd4563a8dcb97aca5
ojchase <ojchase@gmail.com>
2021-02-02 01:21:09 -0700

    Clean up csv reading/writing and have question objects accessible for use.
    Actually writing the file is a webextensions storage api I still need to learn about. Logging it for now.

--- content_scripts/beastify.js
+++ content_scripts/beastify.js
@@ -9,43 +9,61 @@
 	function applyFilter(selectedFilter) {
 		if(selectedFilter === "Test File Read"){
 			// Do stuff
-			filePath = getFilePath("question_data.csv");
-fetch(filePath)
-  .then(response => response.text())
-  .then(text => doStuff(text))
-//  .then(text => alert(text))
-  // outputs the content of the text file
+			readQuestionConfig().then(function(headerAndQuestions){
+				console.log(headerAndQuestions);
+				let questionCategories = headerAndQuestions.questionCategories;
+				let questions = headerAndQuestions.questions;
+				console.log(questionCategories);
+				console.log(questions);
+				for(question of questions){
+					console.log(`${questionCategories[1]} question?: [${question[questionCategories[1]]}] [${question.QuestionText}]`);
+				}
+				let newQuestion = createQuestion("This is a test question", "Spiritual", "FALSE");
+				questions.push(newQuestion);
+				saveQuestions(questionCategories, questions);
+			});
+			console.log("returning from applyfilter");
 			return;
 		}
 		alert(`Applying filter: ${selectedFilter}`);
 	}
 
-function doStuff(text){
-	let x = Papa.parse(text, {header: true, skipEmptyLines: true});
-	console.log(x);
-	console.log(`There are ${x.data.length} items in the question set`);
-	let headers = x.meta.fields;
-	for(question of x.data){
-		console.log(`${headers[1]} question?: ${question[headers[1]]} (${question.QuestionText}`);
+	// Returns a Promise<{questionCategories: string[], questions: object[]}>
+	function readQuestionConfig(){
+		let filePath = getFilePath("question_data.csv");
+		return fetch(filePath)
+			.then(response => response.text())
+			.then((text) => {
+				console.log("starting to parse text");
+				let papaParsedObject = Papa.parse(text, {header: true, skipEmptyLines: true});
+				let headers = papaParsedObject.meta.fields;
+				console.log(papaParsedObject);
+				return {
+					questionCategories: headers,
+					questions: papaParsedObject.data
+				}
+			});
 	}
 	
-	
-	let newQuestionText = "this is a test question";
-	let newQuestionSpiritual = "FALSE";
-	let newQuestion = {
-		
+	function getQuestionByText(questions, text){
+		return questions.find(q => q.QuestionText === text);
 	}
-	newQuestion[headers[0]] = newQuestionText;
-	newQuestion[headers[1]] = newQuestionSpiritual;
-	x.data.push(newQuestion);
 	
-	let y = Papa.unparse({
-		fields: headers,
-		data: x.data
-	},{headers: true});
-	console.log("New file:")
-	console.log(y);
-}
+	function createQuestion(question, category, value){
+		let newQuestion = {};
+		newQuestion["QuestionText"] = question;
+		newQuestion[category] = value;
+		return newQuestion;
+	}
+
+	function saveQuestions(questionCategories, questions){
+		let csvString = Papa.unparse({
+			fields: questionCategories,
+			data: questions
+		},{headers: true});
+		console.log("New file:")
+		console.log(csvString);
+	}
 
   /**
    * Remove every beast from the page.
