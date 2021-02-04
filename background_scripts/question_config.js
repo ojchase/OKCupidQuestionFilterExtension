889192a682c3854cd7f2a26d833e49de09112fe0
ojchase <ojchase@gmail.com>
2021-02-04 00:09:38 -0700

    Move the CSV stuff, to eventually be all the question knowledge, to the backup script

--- background_scripts/question_config.js
+++ background_scripts/question_config.js
@@ -1,1 +1,52 @@
-console.log("This is a background script");
+readQuestionConfig().then(function(headerAndQuestions){
+	let questionCategories = headerAndQuestions.questionCategories;
+	let questions = headerAndQuestions.questions;
+	let newQuestion = createQuestion("This is a test question", "Spiritual", "FALSE");
+	questions.push(newQuestion);
+	saveQuestions(questionCategories, questions);
+});
+
+
+function getFilePath(fileName){
+	return browser.extension.getURL(fileName);
+}
+
+// Returns a Promise<{questionCategories: string[], questions: object[]}>
+function readQuestionConfig(){
+	let filePath = getFilePath("question_data.csv");
+	return fetch(filePath)
+		.then(response => response.text())
+		.then((text) => {
+			let papaParsedObject = Papa.parse(text, {header: true, skipEmptyLines: true});
+			let headers = papaParsedObject.meta.fields;
+			return {
+				questionCategories: headers,
+				questions: papaParsedObject.data
+			}
+		});
+}
+
+function getQuestionByText(questions, text){
+	return questions.find(q => q.QuestionText === text);
+}
+
+function getQuestionTextsByCategoryAndValue(questions, category, value){
+	return questions.filter(q => q[category] === value)
+		.map(q => q.QuestionText);
+}
+
+function createQuestion(question, category, value){
+	let newQuestion = {};
+	newQuestion["QuestionText"] = question;
+	newQuestion[category] = value;
+	return newQuestion;
+}
+
+function saveQuestions(questionCategories, questions){
+	let csvString = Papa.unparse({
+		fields: questionCategories,
+		data: questions
+	},{headers: true});
+	console.log("New file:")
+	console.log(csvString);
+}
--- content_scripts/beastify.js
+++ content_scripts/beastify.js
@@ -8,70 +8,7 @@
 	 * Applies the given filter
 	 */
 	function applyFilter(selectedFilter) {
-		if(selectedFilter === "Test File Read"){
-			// Do stuff
-			readQuestionConfig().then(function(headerAndQuestions){
-				let questionCategories = headerAndQuestions.questionCategories;
-				let questions = headerAndQuestions.questions;
-				let newQuestion = createQuestion("This is a test question", "Spiritual", "FALSE");
-				questions.push(newQuestion);
-				saveQuestions(questionCategories, questions);
-			});
-			return;
-		}
-		if(selectedFilter === "Spiritual"){
-			readQuestionConfig().then(function(headerAndQuestions){
-				let questionCategories = headerAndQuestions.questionCategories;
-				let questions = headerAndQuestions.questions;
-				let desiredQuestions = getQuestionTextsByCategoryAndValue(questions, "Spiritual", "TRUE");
-				jq(`div.profile-question`).hide();
-				for (var q of desiredQuestions){
-					jq(`div.profile-question:has(button.profile-question-content:has(h3:contains("${q}")))`).show();
-				}
-			});
-			return;
-		}
 		alert(`Applying filter: ${selectedFilter}`);
-	}
-
-	// Returns a Promise<{questionCategories: string[], questions: object[]}>
-	function readQuestionConfig(){
-		let filePath = getFilePath("question_data.csv");
-		return fetch(filePath)
-			.then(response => response.text())
-			.then((text) => {
-				let papaParsedObject = Papa.parse(text, {header: true, skipEmptyLines: true});
-				let headers = papaParsedObject.meta.fields;
-				return {
-					questionCategories: headers,
-					questions: papaParsedObject.data
-				}
-			});
-	}
-	
-	function getQuestionByText(questions, text){
-		return questions.find(q => q.QuestionText === text);
-	}
-	
-	function getQuestionTextsByCategoryAndValue(questions, category, value){
-		return questions.filter(q => q[category] === value)
-			.map(q => q.QuestionText);
-	}
-	
-	function createQuestion(question, category, value){
-		let newQuestion = {};
-		newQuestion["QuestionText"] = question;
-		newQuestion[category] = value;
-		return newQuestion;
-	}
-
-	function saveQuestions(questionCategories, questions){
-		let csvString = Papa.unparse({
-			fields: questionCategories,
-			data: questions
-		},{headers: true});
-		console.log("New file:")
-		console.log(csvString);
 	}
 
   /**
