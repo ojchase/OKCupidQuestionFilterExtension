e1d39bb4eeae8e7ce23458c3bc4b5bd5dd6d21d3
ojchase <ojchase@gmail.com>
2021-02-07 03:03:10 -0700

    Very much in progress attempt at watching the page load and finding the questions column I need.
    The MutationObserver isn't detecting when '.page-section' gets '.questions' though, on a fresh load. (The non-reload path where the extension is loaded and a page is already up works great!) Attaching the observer up one layer shows a bunch.

--- content_scripts/filter.js
+++ content_scripts/filter.js
@@ -5,7 +5,134 @@ if (window.hasRunOKCupidQuestionFilterExtensionFilter) {
 window.hasRunOKCupidQuestionFilterExtensionFilter = true;
   
 document.body.style.border = "5px solid red";
+console.log("Script is running");
 var jq = jQuery.noConflict();
+jq(document).ready(doStuff);
+
+function doStuff(){
+	entry("doStuff");
+	
+	const jqElement = jq('#root');
+	waitForElementToExist(jqElement, "main")
+		.then((result) => {
+			return waitForElementToExist(result, '.page-section');
+		})
+		.then((result) => {
+			return waitForElementToExist(result, '.questions');
+		})
+		.then((result) => {
+			return waitForElementToExist(result, '.profile-questions-main');
+		})
+		.then((result) => {
+			return waitForElementToExist(result, '.profile-questions-column--filters');
+		})
+		.then((result) => {
+			return waitForElementToExist(result, '.profile-questions-sidebar');
+		})
+		.then((result) => {
+			return waitForElementToExist(result, '.profile-questions-filters');
+		})
+		.then((result) => {
+			return waitForElementToExist(result, '.profile-questions-filters-inner');
+		})
+		.then((result) => {
+			console.log("Promise worked");
+			console.log(result);
+		})
+		.catch((e) => {
+			console.log("Promise timed out"); 
+			console.log(e);
+		});
+	exit("doStuff");
+}
+
+function getCurrentChildIfPresent(existingJqueryObject, desiredChildElementSelector){
+	let result;
+					//console.log("about to log element");
+					//listJqueryItems(existingJqueryObject);
+					//console.log("about to log all children");
+					//existingJqueryObject.children().each((index, child) => {
+					//	console.log(child);
+					//});
+					//console.log("about to log children that match selector");
+					//existingJqueryObject.children(desiredChildElementSelector).each((index, child) => {
+					//	console.log(child);
+					//});
+	let matchingChildren = existingJqueryObject.children(desiredChildElementSelector);
+	if(matchingChildren.length > 0){
+		console.log(matchingChildren.first());
+		return matchingChildren.first();
+	}
+	return null;
+}
+
+// Returns promise<Jquery>
+function waitForElementToExist(existingJqueryObject, desiredChildElementSelector) {
+	entry("waitForElementToExist");
+	// https://blog.frankmtaylor.com/2017/06/16/promising-a-mutation-using-mutationobserver-and-promises-together/
+	const rejectTime = 30000;
+	let result = getCurrentChildIfPresent(existingJqueryObject, desiredChildElementSelector);
+	if(result){
+		exit("waitForElementToExist");
+		return Promise.resolve(result);
+	}
+	exit("waitForElementToExist");
+	return new Promise((resolve,reject) => {
+		entry("New promise");
+		//if exists, resolve
+		let hasChanged = false;
+		const observerConfig = {
+			childList: true,
+			subtree: true,
+			attributes: true,
+			characterData: true
+		};       
+		const observer = new MutationObserver((mutations) => {
+			entry("ObservingMutations");
+			mutations.forEach((mutation) => {
+				mutation.addedNodes.forEach((addedNode) => {
+					console.log(`Found a new element! ${addedNode}`);
+					console.dir(addedNode);
+				});
+				mutation.removedNodes.forEach((removedNode) => {
+					console.log(`Found a removed element! ${removedNode}`);
+					console.dir(removedNode);
+				});
+				result = getCurrentChildIfPresent(existingJqueryObject, desiredChildElementSelector);
+				//if (mutation.ELEMENT_NAME == desiredChildElementSelector) {
+				if (result) {
+					hasChanged = true;
+					observer.disconnect();
+					resolve(result);
+				}
+			});
+			exit("ObservingMutations");
+		});
+		
+		window.setTimeout(()=>{
+			if (!hasChanged) {
+				reject("Yikes");
+			}
+		},rejectTime);
+		//existingJqueryObject = jq('main');
+		existingJqueryObject.each( (index, domObject) => {
+			entry("Observing domObject");
+			console.log(domObject);
+			existingJqueryObject.children().each((index, child) => {console.log(child)});
+			observer.observe(domObject, observerConfig);
+			exit("Observing domObject");
+		});
+		//observer.observe(existingElement, observerConfig);
+		exit("New promise");
+	});
+}
+
+function listJqueryItems(jqSelector){
+	entry("listJqueryItems");
+	jq(jqSelector).each((index, child) => {console.log(child)});
+	exit("listJqueryItems");
+}
+
 function addBorders(){
 	entry("addBorders");
 	jq(`div.profile-question`).css('border', '3px solid blue')
@@ -63,6 +190,5 @@ function createFilterButtons() {
 	newButton.appendTo('div.profile-questions-filters-inner');
 }
 
-addObserverToQuestionList();
 
 })();
