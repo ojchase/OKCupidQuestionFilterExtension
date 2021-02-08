960df0900d0bcca39c451f1e171f9e115ee6ce49
ojchase <ojchase@gmail.com>
2021-02-08 01:39:12 -0700

    Completely rework loading on the page.
    The previoius approach didn't work. OKCupid does too much DOM manipulation. I couldn't trust the element I was waiting on would still exist, and so I'd miss the mutations I wanted.
    
    This goes back to the old approach, but a bit more elegantly, and with promises. It listens for the correct items. Not just the elements on the page, but also the absence of any loading info.
    
    Also does some cleanup. Much (all?) from the previous commit removed, and some from before then. I think everything left here is important.
    
    A bit of restructuring to make future dev easier.

--- content_scripts/filter.js
+++ content_scripts/filter.js
@@ -4,179 +4,71 @@ if (window.hasRunOKCupidQuestionFilterExtensionFilter) {
 }
 window.hasRunOKCupidQuestionFilterExtensionFilter = true;
   
-document.body.style.border = "5px solid red";
 console.log("Script is running");
 var jq = jQuery.noConflict();
-jq(document).ready(doStuff);
+jq(document).ready(manipulatePage);
 
-function doStuff(){
-	entry("doStuff");
-	
-	const jqElement = jq('#root');
-	waitForElementToExist(jqElement, "main")
-		.then((result) => {
-			return waitForElementToExist(result, '.page-section');
-		})
-		.then((result) => {
-			return waitForElementToExist(result, '.questions');
-		})
-		.then((result) => {
-			return waitForElementToExist(result, '.profile-questions-main');
-		})
-		.then((result) => {
-			return waitForElementToExist(result, '.profile-questions-column--filters');
-		})
-		.then((result) => {
-			return waitForElementToExist(result, '.profile-questions-sidebar');
-		})
-		.then((result) => {
-			return waitForElementToExist(result, '.profile-questions-filters');
-		})
-		.then((result) => {
-			return waitForElementToExist(result, '.profile-questions-filters-inner');
-		})
-		.then((result) => {
-			console.log("Promise worked");
-			console.log(result);
-		})
-		.catch((e) => {
-			console.log("Promise timed out"); 
-			console.log(e);
-		});
-	exit("doStuff");
-}
-
-function getCurrentChildIfPresent(existingJqueryObject, desiredChildElementSelector){
-	let result;
-					//console.log("about to log element");
-					//listJqueryItems(existingJqueryObject);
-					//console.log("about to log all children");
-					//existingJqueryObject.children().each((index, child) => {
-					//	console.log(child);
-					//});
-					//console.log("about to log children that match selector");
-					//existingJqueryObject.children(desiredChildElementSelector).each((index, child) => {
-					//	console.log(child);
-					//});
-	let matchingChildren = existingJqueryObject.children(desiredChildElementSelector);
-	if(matchingChildren.length > 0){
-		console.log(matchingChildren.first());
-		return matchingChildren.first();
-	}
-	return null;
-}
-
-// Returns promise<Jquery>
-function waitForElementToExist(existingJqueryObject, desiredChildElementSelector) {
-	entry("waitForElementToExist");
-	// https://blog.frankmtaylor.com/2017/06/16/promising-a-mutation-using-mutationobserver-and-promises-together/
-	const rejectTime = 30000;
-	let result = getCurrentChildIfPresent(existingJqueryObject, desiredChildElementSelector);
-	if(result){
-		exit("waitForElementToExist");
-		return Promise.resolve(result);
-	}
-	exit("waitForElementToExist");
-	return new Promise((resolve,reject) => {
-		entry("New promise");
-		//if exists, resolve
-		let hasChanged = false;
-		const observerConfig = {
-			childList: true,
-			subtree: true,
-			attributes: true,
-			characterData: true
-		};       
-		const observer = new MutationObserver((mutations) => {
-			entry("ObservingMutations");
-			mutations.forEach((mutation) => {
-				mutation.addedNodes.forEach((addedNode) => {
-					console.log(`Found a new element! ${addedNode}`);
-					console.dir(addedNode);
-				});
-				mutation.removedNodes.forEach((removedNode) => {
-					console.log(`Found a removed element! ${removedNode}`);
-					console.dir(removedNode);
-				});
-				result = getCurrentChildIfPresent(existingJqueryObject, desiredChildElementSelector);
-				//if (mutation.ELEMENT_NAME == desiredChildElementSelector) {
-				if (result) {
-					hasChanged = true;
-					observer.disconnect();
-					resolve(result);
-				}
-			});
-			exit("ObservingMutations");
-		});
-		
-		window.setTimeout(()=>{
-			if (!hasChanged) {
-				reject("Yikes");
-			}
-		},rejectTime);
-		//existingJqueryObject = jq('main');
-		existingJqueryObject.each( (index, domObject) => {
-			entry("Observing domObject");
-			console.log(domObject);
-			existingJqueryObject.children().each((index, child) => {console.log(child)});
-			observer.observe(domObject, observerConfig);
-			exit("Observing domObject");
-		});
-		//observer.observe(existingElement, observerConfig);
-		exit("New promise");
+function manipulatePage(){
+	document.body.style.border = "5px solid red";
+	waitForPageToLoad('.page-loading, .isLoading').then(() => {
+		listenForQuestionListUpdates();
+		createFilterButtons();
+		manipulateQuestionElements();
+	}).catch(() => {
+		console.log("It's not loaded!")
 	});
 }
 
-function listJqueryItems(jqSelector){
-	entry("listJqueryItems");
-	jq(jqSelector).each((index, child) => {console.log(child)});
-	exit("listJqueryItems");
+
+// Promise
+//Note that the current design has this script active on all okcupid.com pages,
+//not just the questions page, as I can't detect the questions page being up if
+//it was launched via okcupid internal links.
+//See https://stackoverflow.com/questions/20865581/chrome-extension-content-script-not-loaded-until-page-is-refreshed
+async function waitForPageToLoad(selector){
+	return new Promise(async (resolve,reject) => {
+		let loaded = false;
+		let timedOut = false;
+		const timeoutId = window.setTimeout(()=>{
+			if(loaded){
+				return;
+			}
+			timedOut = true;
+			reject();
+		},30000);
+		while(!isPageLoaded(selector) && !timedOut){
+			await new Promise((resolveSimpleTimeout) => {
+				window.setTimeout(() => {
+					resolveSimpleTimeout();
+				}, 500);
+			});
+		}
+		window.clearTimeout(timeoutId);
+		loaded = true;
+		resolve();
+	});
+}
+
+function isPageLoaded(selector){
+	return jq(selector).length === 0;
+}
+
+function manipulateQuestionElements(){
+	addBorders();
 }
 
 function addBorders(){
-	entry("addBorders");
 	jq(`div.profile-question`).css('border', '3px solid blue')
-	exit("addBorders");
 }
 
 function listenForQuestionListUpdates(){
-	entry("listenForQuestionListUpdates");
 	// Thanks to https://stackoverflow.com/a/42805882/1541186 for the approach here
 	var target = document.querySelector('div.profile-questions');
 	var observer = new MutationObserver(function(mutations) {
-		addBorders();
+		manipulateQuestionElements();
 	});
 	var config = { childList: true};
 	observer.observe(target, config);
-	exit("listenForQuestionListUpdates");
-}
-
-function addObserverToQuestionList() {
-	entry("addObserverToQuestionList");
-	// Thanks to https://stackoverflow.com/a/40418394/1541186 for the approach
-	var questionList = document.querySelector("div.profile-questions");
-	if(!questionList) {
-		//The node we need does not exist yet.
-		//Wait 2s and try again
-		//Note that the current design has this script active on all okcupid.com pages,
-		//not just the questions page, as I can't detect the questions page being up if
-		//it was launched via okcupid internal links.
-		//See https://stackoverflow.com/questions/20865581/chrome-extension-content-script-not-loaded-until-page-is-refreshed
-		window.setTimeout(addObserverToQuestionList,2000);
-		return;
-	}
-	listenForQuestionListUpdates();
-	createFilterButtons();
-	addBorders();
-	exit("addObserverToQuestionList");
-}
-
-function entry(method){
-	console.log(`ENTERED ${method}`);
-}
-
-function exit(method){
-	console.log(`EXITED ${method}`);
 }
 
 function createFilterButtons() {
