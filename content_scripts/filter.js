e28d98191d9e8888d50cbc1fdfa9f5ee813816f9
ojchase <ojchase@gmail.com>
2021-02-12 23:53:22 -0700

    Change throttling the save to debouncing, using MIT licensed underscore.js

--- content_scripts/filter.js
+++ content_scripts/filter.js
@@ -258,24 +258,12 @@ function addQuestion(questions, questionText){
 	questions.push(newQuestion);
 }
 
-const saveQuestions = throttle(function(questions){
+const saveQuestions = _.debounce(function(questions) {
 	let savePromise = browser.runtime.sendMessage({
 		"queryType": "SaveQuestions",
 		"updatedQuestions": questions
 	});
 	return savePromise.catch(logFailureResponse).then(logSuccessResponse);
 }, 5000);
-
-// Thanks to https://stackoverflow.com/a/59378445/ (which is really https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_throttle)
-function throttle(func, timeFrame) {
-  var lastTime = 0;
-  return function (...args) {
-      var now = new Date();
-      if (now - lastTime >= timeFrame) {
-          func(...args);
-          lastTime = now;
-      }
-  };
-}
 
 })();
