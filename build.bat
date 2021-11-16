8509d1d29726cbb42ad539022993d07c898bede6
ojchase <ojchase@gmail.com>
2021-11-16 01:03:18 -0700

    Build script output file name update for consistency

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 set version=%1
-"C:\Program Files\7-Zip\7z.exe" a OKCupidFilterExtension_%version%.zip background_scripts content_scripts icons lib manifest.json README.md
+"C:\Program Files\7-Zip\7z.exe" a OKCupidQuestionFilterExtension_%version%.zip background_scripts content_scripts icons lib manifest.json README.md
